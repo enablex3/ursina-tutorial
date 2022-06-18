f982914ebae431535f12b7adbb437fcc08574676
enblex3 <ereyes@citadel.edu>
2022-06-18 04:20:59 -0400

    maze tutorial

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
-stone_floor = "textures/ground/stone_floor/tiled_basecolor.jpg"
-
-MAXIMUM_GRAB_DISTANCE = 3
+MAZE_SCALE = 5
+PLAYER_STARTING_POSITION = (-28.889, 0, 16.485)
 
 def update():
-    global grab_entity
+    if player.y < 0:
+        player.position = PLAYER_STARTING_POSITION
 
-    if cube.hovered:
-
-        if distance(player, cube) <= MAXIMUM_GRAB_DISTANCE:
-            grab_entity = True
-        else:
-            grab_entity = False
-
-    else:
-        grab_entity = False
-
-    if grab_entity:
-        if mouse.left:
-            cube.world_position += player.cursor.world_position
-grab_entity = False
-
-window.show_ursina_splash = True
+def input(key):
+    if key == "escape":
+        quit()
 
 app = Ursina()
 
-ground = Entity(model="plane", texture=stone_floor, collider="mesh", scale=(100, 1, 100))
-cube = Entity(model="cube", color=color.olive, collider="mesh", scale=(1, 1, 1), position=(0, 0.5, 0))
+maze = Entity(model="models/maze", texture="brick", scale=MAZE_SCALE, collider="mesh")
 
-Sky()
-
-player = FirstPersonController()
-
+player = FirstPersonController(position=PLAYER_STARTING_POSITION)
 app.run()
